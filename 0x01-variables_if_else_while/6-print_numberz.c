bf32cf9c58a94f739d61bea6308382e4e4d81f69
sengineeralx <sengineeralx@gmail.com>
2023-02-21 05:53:54 -0800

    6-print_numberz.c git commit -m 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <time.h>
+
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+
+}
