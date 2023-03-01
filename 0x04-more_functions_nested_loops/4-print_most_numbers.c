48520164f085c95fc33de262ce5759f06355af6a
sengineeralx <sengineeralx@gmail.com>
2023-03-01 00:53:13 -0800

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,18 +10,18 @@ void print_most_numbers(void)
 
 {
 
-        int x = 0;
+	int x = 0;
 
-        for (; x <= 9; x++)
-        {
-        if (x == 2 || x == 4)
-        {
-        continue;
-        }
-        else
-        {
-        _Putchar(x + '0');
-        }
-        }
-	_Putchar('\n');
+	for (; x <= 9; x++)
+	{
+	if (x == 2 || x == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(x + '0');
+	}
+	}
+	_putchar('\n');
 }
